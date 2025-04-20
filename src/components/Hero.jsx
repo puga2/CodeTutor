bd7e61f2565b7e92ceab4c1975959477459d74f0
GaGa <o.kimhong168@gmail.com>
2025-04-20 14:22:52 +0700

    dooter and trynow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,9 +25,9 @@ const Hero = () => {
                         As the first-ever website experience platform, CodeTutor less let's marketers, designer, and developers come together to build,manage , and optimize web experiences that get results.
                     </p>
                     <div  className='flex sm:flex-row gap-8 shrink-0'>
-                        <Link to="/start-building" className="bg-[#4353ff] hover:bg-blue-700 px-8 py-4 text-lg font-medium transition-colors rounded-sm text-white duration-200" >
+                        <Link to="/start-building" className="bg-[#4353ff] hover:bg-blue-700 px-8 py-4 text-md md:text-lg font-medium transition-colors rounded-sm text-white duration-200" >
                         Start building</Link>
-                        <Link to="/start-building" className=" hover:text-white/40 flex items-center px-8 group py-4 text-lg font-medium transition-colors rounded-sm text-white duration-200" >
+                        <Link to="/start-building" className=" hover:text-white/40 flex items-center px-8 group py-4 text-md md:text-lg font-medium transition-colors rounded-sm text-white duration-200" >
                           Contact Sales <HiArrowRight className='ml-3 w-5 h-5 group-hover:translate-x-1 transition-transform'  />
                         </Link>
                     </div>
--- src/components/TryNow.jsx
+++ src/components/TryNow.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { HiArrowRight } from "react-icons/hi2";
+import { Link } from "react-router";
 
 const TryNow = () => {
   return (
-    <div>TryNow</div>
-  )
-}
+    <div className="bg-black text-white py-20 ">
+      <div className="primary-container">
+        <div className="flex flex-col  md:flex-row justify-between gap-16">
+          {/* left */}
+          <div className="md:w-3/5">
+            <div className="space-y-5">
+              <h2 className="text-4xl md:text-7xl font-bold mb-8">
+                Try it for <br />
+                free
+              </h2>
+              <p>
+                Try CodeTutor for as long as you like with our free Starter
+                plan. Purchase a paid Site plan to publish , host, and unlock
+                additinal features.
+              </p>
+              <Link
+                to="/get-started"
+                className="hover:text-gray-300 bg-blue-600 px-4 py-2 rounded-md
+            hover:bg-blue-700 "
+              >
+                Get Started - it's free
+              </Link>
+            </div>
+          </div>
 
-export default TryNow
+          {/* right */}
+          <div className="md:w-2/5 space-y-12">
+            <div className="">
+              <h3 className="text-lg md:text-xl leading-relaxed font-medium mb-4">
+                Try CodeTutor for as long as you like with our free Starter
+                plan.Purhcase a paid site palin to publish,host ,and unlock
+                additional features.
+              </h3>
+            </div>
+            <div className="space-y-2">
+                <h2 className="text-xl font-black">
+                    Contact sales
+                </h2>
+                <p>Interested in CodeTutor Enterpriese ? Get in touch with our sales team to receive a free personalized demo.</p>
+                    <Link className="inline-flex items-center text-white hover:text-gray-300 hover:underline  transition-colors">
+                            Discover AI at CodeTutor
+                                <HiArrowRight className="ml-2  w-5 h-5" />
+                    </Link>
+            </div>
+            <div className="space-y-2">
+                <h2 className="text-xl font-black">
+                Hire  a Certified Partner
+                </h2>
+                <p>Interested in CodeTutor Enterpriese ? Get in touch with our sales team to receive a free personalized demo.</p>
+                    <Link className="inline-flex items-center text-white hover:text-gray-300 hover:underline  transition-colors">
+                            Browse Partner
+                                <HiArrowRight className="ml-2  w-5 h-5" />
+                    </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TryNow;
